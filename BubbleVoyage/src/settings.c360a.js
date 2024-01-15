8697975bb9ab63faf8820654115213faf4adabc9
rzon <ronzong@gmail.com>
2024-01-15 19:32:33 +0800

    1

---
+++ BubbleVoyage/src/settings.c360a.js
@@ -0,0 +1,19 @@
+window._CCSettings = {
+    platform: "fb-instant-games",
+    groupList: ["default"],
+    collisionMatrix: [
+        [true]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/loading.fire",
+    orientation: "portrait",
+    jsList: [],
+    bundleVers: {
+        internal: "d17e2",
+        resources: "a654f",
+        main: "4fd81"
+    }
+};
