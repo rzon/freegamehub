7edb20fb39e78a02fe82ce6f51b59f38e6c2bab0
jqhong <jqhong@onlyou.com>
2024-02-06 11:58:42 +0800

    添加从 ajax 获取 csv 内容并写到首页上

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+/**
+ * 判断是移动端, 还是 pc 端
+ * @returns {*}
+ */
 function getDeviceType() {
     var userAgent = navigator.userAgent;
     if(/Mobi|Android/i.test(userAgent)) {
@@ -7,6 +11,10 @@ function getDeviceType() {
     }
 }
 
+/**
+ * 获取 csv 游戏内容
+ * @returns {Array}
+ */
 function getCsvData() {
     // /assets/excel/game_list.csv
     var response;
@@ -36,7 +44,10 @@ function getCsvData() {
         if (li.length > 0) {
             for (var i = 1; i < li.length; i++) {
                 if (li[i]) {
-                    gameUrlLi.push(li[i].split(','));
+                    var itemLi = li[i].split(',');
+                    if (itemLi.length >= 2) {
+                        gameUrlLi.push(itemLi);
+                    }
                 }
             }
         }
@@ -45,6 +56,91 @@ function getCsvData() {
     return [];
 }
 
-$(document).ready(function(){
+/**
+ * 追加 cardTemplateHtml
+ * @param resultHtmlLi
+ * @param nineGridItemTemplateHtmlLi
+ */
+function appendCardTemplateHtml(resultHtmlLi, nineGridItemTemplateHtmlLi) {
+    var cardTemplateHtml = $("#cardTemplate").html();
+    cardTemplateHtml = cardTemplateHtml.replace(/{nineGridItemContent}/g, nineGridItemTemplateHtmlLi.join(""));
+    resultHtmlLi.push(cardTemplateHtml);
+}
 
+/**
+ * 添加 PopularGame div
+ * @param resultHtmlLi
+ */
+function appendPopularGameTemplateHtml(resultHtmlLi) {
+    var popularGameTemplateHtml = $("#popularGameTemplate").html();
+    resultHtmlLi.push(popularGameTemplateHtml);
+}
+
+/**
+ * 追加 GoogleAdsTemplate
+ * @param resultHtmlLi
+ */
+function appendGoogleAdsTemplate(resultHtmlLi) {
+    var googleAdsTemplateHtml = $("#googleAdsTemplate").html();
+    googleAdsTemplateHtml = googleAdsTemplateHtml.replace(/{adsbygoogleScriptContent}/g, adsbygoogleScript);
+    googleAdsTemplateHtml = googleAdsTemplateHtml.replace(/{adsbygoogleBottomScriptContent}/g, adsbygoogleBottomScript);
+    resultHtmlLi.push(googleAdsTemplateHtml);
+}
+
+/**
+ * csv 数据添加到 html 中
+ */
+function appendCsvDataToHtml() {
+    var gameUrlLi = getCsvData();
+    var resultHtmlLi = [];
+
+    var nineGridItemTemplateHtmlLi = [];
+
+    var itemCount = 9;
+
+    var isAddPopularGame = false;
+    var isAddGoogleAds = false;
+
+    for (var i = 0; i < gameUrlLi.length; i++) {
+        var nineGridItemTemplateHtml = $("#nineGridItemTemplate").html();
+        nineGridItemTemplateHtml = nineGridItemTemplateHtml.replace(/{gameUrl}/g, gameUrlLi[i][0].trim());
+        nineGridItemTemplateHtml = nineGridItemTemplateHtml.replace(/{gamePic}/g, gameUrlLi[i][1].trim());
+
+        nineGridItemTemplateHtmlLi.push(nineGridItemTemplateHtml);
+
+        if (i == 8) {
+            appendCardTemplateHtml(resultHtmlLi, nineGridItemTemplateHtmlLi);
+            nineGridItemTemplateHtmlLi = [];
+
+            // popularGameTemplate
+            appendPopularGameTemplateHtml(resultHtmlLi);
+            isAddPopularGame = true;
+        } else if (nineGridItemTemplateHtmlLi.length == itemCount) {
+            appendCardTemplateHtml(resultHtmlLi, nineGridItemTemplateHtmlLi);
+            nineGridItemTemplateHtmlLi = [];
+        }
+
+        if (resultHtmlLi.length == 3) {
+            appendGoogleAdsTemplate(resultHtmlLi);
+            isAddGoogleAds = true;
+        }
+    }
+
+    if (nineGridItemTemplateHtmlLi.length > 0) {
+        appendCardTemplateHtml(resultHtmlLi, nineGridItemTemplateHtmlLi);
+        nineGridItemTemplateHtmlLi = [];
+    }
+
+    if (!isAddPopularGame) {
+        appendPopularGameTemplateHtml(resultHtmlLi);
+    }
+    if (!isAddGoogleAds) {
+        appendGoogleAdsTemplate(resultHtmlLi);
+    }
+
+    $("#cardLayoutDiv").html(resultHtmlLi.join(""));
+}
+
+$(document).ready(function(){
+    appendCsvDataToHtml();
 });
