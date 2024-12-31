12c1684308dc8175576825945938c1bbb4ef39fb
rzon <ronzong@gmail.com>
2024-12-31 21:35:22 +0800

    StuntMoto

---
+++ game/StuntMoto/src/settings.js
@@ -0,0 +1,26 @@
+window._CCSettings = {
+    platform: "fb-instant-games",
+    groupList: ["default", "UIGroup", "Map1", "Map2", "Map3", "Map4", "Map5", "Robot2", "Robot3", "Robot4", "Robot5", "BobWeight", "Role"],
+    collisionMatrix: [
+        [true, false, false, null, null, null, null, null, false, null, null, false, true],
+        [false, false, false, null, null, null, null, null, false],
+        [false, false, false],
+        [false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false, false],
+        [false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, true],
+        [false, false, false, false, false, false, false, false, true],
+        [false, false, false, false, false, false, false, false, false, true],
+        [false, false, false, false, false, false, false, false, false, false, true],
+        [false, false, false, false, false, false, false, false, false, false, false, false],
+        [true, false, false, false, false, false, false, false, false, false, false, false, false]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/preload.fire",
+    orientation: "portrait",
+    jsList: ["assets/k-cocos/k-cocos.js"]
+};
